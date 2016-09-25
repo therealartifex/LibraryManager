8e99ddcb03262422b79f91ad0405cdc561e52007
Sceleris Artifex <linux.usaf@live.com>
2016-09-25 18:43:13 -0500

    Layout changes

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(236, 277);
+            this.textBox1.Location = new System.Drawing.Point(245, 142);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(560, 20);
             this.textBox1.TabIndex = 0;
