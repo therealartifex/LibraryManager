338823f2ba44388b42b2a1453f0286e462222dc0
Sceleris Artifex <linux.usaf@live.com>
2016-11-21 20:21:51 -0600

    change data source

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace LibraryManager.Properties {
                 return ((string)(this["LMCon"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\libman4.mdf;I" +
+            "ntegrated Security=True")]
+        public string libman4CS {
+            get {
+                return ((string)(this["libman4CS"]));
+            }
+        }
     }
 }
